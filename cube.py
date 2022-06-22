f1bf1eef90c72fc63a5c926d8ed5883755e4ef8c
Sofia <sofia.manana@sansano.usm.cl>
2022-06-22 14:57:22 -0400

    inicio de teclas, averiguar como se llama el deformar una figura

--- cube.py
+++ cube.py
@@ -25,15 +25,27 @@ def main():
     pg.display.set_mode(display, pg.DOUBLEBUF|pg.OPENGL|pg.RESIZABLE)
     gluPerspective(40,(display[0]/display[1]),1,10)
     glTranslatef(0.0,0.0,-5)
-    
+    glRotatef(0,0,1,1)
 
     while True:
         for event in pg.event.get():
             if (event.type == pg.quit):
                 pg.quit()
                 quit()
-        
-        glRotatef(1,1,1,1)
+
+            if (event.type == pg.MOUSEBUTTONDOWN):
+                glTranslatef(0,0.5,0)
+            if (event.type == pg.MOUSEBUTTONUP):
+                glTranslatef(0,-0.5,0)
+            keys = pg.key.get_pressed()
+            if(keys[K_w]):
+                glTranslatef(0,0.5,0)
+            if(keys[K_a]):
+                glTranslatef(-0.5,0,0)
+            if(keys[K_s]):
+                glTranslatef(0,-0.5,0)
+            if(keys[K_w]):
+                glTranslatef(0.5,0,0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         sq()
         pg.display.flip()
