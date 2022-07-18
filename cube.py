c3a9f2ca503de9417eb4a804f9fa1db70012252a
Sofia <sofia.manana@sansano.usm.cl>
2022-07-18 16:50:56 -0400

    avance en picking

--- cube.py
+++ cube.py
@@ -3,21 +3,27 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 #vertices de las pages
-vertices = [[1,1,1],[1,-1,1],[-1,-1,1],[-1,1,1],
-            [1,1,-1],[1,-1,-1],[-1,-1,-1],[-1,1,-1]]
+vertices = [[1,1],[1,-1],[-1,-1],[-1,1]]
 #arcos entre las pages
-edges = ((0,1),(1,2),(2,3),(3,0),
-        (4,5),(5,6),(6,7),(7,4),
-        (0,4),(1,5),(2,6),(3,7))
+edges = [[0,1],[1,2],[2,3],[3,0]]
+vertices2 = [[1,1.5],[1,-1.5],[-1,-3.5],[-1,3.5]]
+#arcos entre las pages
+edges2 = [(0,1),(1,2),(2,3),(3,0)]
 
-def sq():
+def sq(vertices, edges):
     glBegin(GL_LINES)
     
     for e in edges:
         for vertex in e:
-            glVertex3iv(vertices[vertex])
+            glVertex2fv(vertices[vertex])
     
     glEnd()
+
+def norm(x, y):
+    temp_y = y*(-1)
+    x_norm = x - 250
+    y_norm = temp_y + 250
+    return x_norm/250, y_norm/250
 
 def main():
     pg.init()
@@ -45,7 +51,8 @@ def main():
             if(keys[K_w]):
                 glTranslatef(0.5,0,0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        sq()
+        sq(vertices,edges)
+        sq(vertices2,edges2)
         pg.display.flip()
         pg.time.wait(10)
 
